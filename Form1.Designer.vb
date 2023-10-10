d655371c18388be6377ca0861dbd3e2b49712b4e
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 09:26:40 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(340, 305)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(222, 73)
+        Button1.TabIndex = 0
+        Button1.Text = "Compute"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(262, 105)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(77, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Number"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(262, 184)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(140, 25)
+        Label2.TabIndex = 2
+        Label2.Text = "Exponetial Value"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(435, 105)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.MistyRose
+        TextBox2.Location = New Point(435, 181)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(285, 31)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Maths Functions"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Text = Math.Exp(Val(TextBox1.Text))
+
+
+        'Dim number, exponent As Double
+
+        'number = Val(TextBox1.Text)
+        'exponent = Math.Exp(number)
+        'TextBox2.Text = exponent
+    End Sub
+End Class
